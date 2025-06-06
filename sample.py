43e38112a73a909a5d105684b57a2547ae6ddc17
waiorecchi <tatukikitamura123456@gmail.com>
2025-06-06 12:46:27 +0900

    完成

---
+++ sample.py
@@ -0,0 +1,16 @@
+import requests
+##<img src="https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png" alt="Your Image Badge" />
+##https://tryhackme.com/api/v2/badges/public-profile?userPublicId=4877687
+api_url = "https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png"
+
+
+res = requests.get(api_url)
+
+
+def save_image(filename, image):
+    with open(filename, "wb") as fout:
+        fout.write(image)
+
+print(res.content)
+
+save_image("assets/thm_propic.svg",res.content)
