36d26989a9f14868319ef22a99c1910e8889ba27
wairecchi <tatukikitamura123456@gmail.com>
2025-06-11 20:46:44 +0900

    a

--- sample.py
+++ sample.py
@@ -1,16 +1,16 @@
 import requests
-##<img src="https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png" alt="Your Image Badge" />
-##https://tryhackme.com/api/v2/badges/public-profile?userPublicId=4877687
-api_url = "https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png?update=4"
-#post_url = "https://tryhackme.com/api/v2/badges/public-profile/image"
-#data = {userPublicId: 4877687, username: "tatsukikitamura"}
+import time
 
-#preresponse = requests.post(post_url,params=data)
-res = requests.get(api_url)
+def update_thm_badge():
+    # キャッシュ回避用にタイムスタンプを付与
+    api_url = f"https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png?ts={int(time.time())}"
+    
+    res = requests.get(api_url)
+    if res.status_code == 200:
+        with open("assets/thm_propic.png", "wb") as f:
+            f.write(res.content)
+        print("画像を更新しました")
+    else:
+        print(f"エラー: {res.status_code}")
 
-
-def save_image(filename, image):
-    with open(filename, "wb") as fout:
-        fout.write(image)
-
-save_image("assets/thm_propic.png",res.content)
+update_thm_badge()
