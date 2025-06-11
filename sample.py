c5b33d94a1d38281fe82b74c40ae2ca437feba11
おれ <102579111+waiorecchi@users.noreply.github.com>
2025-06-11 13:37:33 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,8 +2,10 @@ import requests
 ##<img src="https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png" alt="Your Image Badge" />
 ##https://tryhackme.com/api/v2/badges/public-profile?userPublicId=4877687
 api_url = "https://tryhackme-badges.s3.amazonaws.com/tatsukikitamura.png"
+post_url = "https://tryhackme.com/api/v2/badges/public-profile/image"
+data = {userPublicId: 4877687, username: "tatsukikitamura"}
 
-
+preresponse = requests.post(post_url,data=data)
 res = requests.get(api_url)
 
 
